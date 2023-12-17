ebfc42d6ce0e771effb2f6e4d6cd345aaed5fad9
MK1 <shin.m.dp@gmail.com>
2023-12-17 22:32:07 +0200

    loginViewController 1.3

---
+++ VoioM/Controllers/FavoritesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesViewController.swift
+//  VoioM
+//
+//  Created by SHIN MIKHAIL on 17.12.2023.
+//
+
+import Foundation
---
+++ VoioM/Controllers/MainViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController.swift
+//  VoioM
+//
+//  Created by SHIN MIKHAIL on 17.12.2023.
+//
+
+import Foundation
